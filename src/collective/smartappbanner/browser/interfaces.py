4cf65a2032d08b0b0828bbb01e140899c9196975
Maurits van Rees <maurits@vanrees.org>
2017-05-11 17:22:21 +0200

    Improved the settings. All are optional now.
    Added button_text field.
    Made platforms a list for choosing android and/or ios. Previously it was a textline with string 'android,ios'.

--- src/collective/smartappbanner/browser/interfaces.py
+++ src/collective/smartappbanner/browser/interfaces.py
@@ -4,37 +4,56 @@ from zope import schema
 from zope.interface import Interface
 
 
+PLATFORMS = [
+    u'android',
+    u'ios',
+]
+
+
 class ISmartappbannerBaseSettings(Interface):
     """Smart app banner settings """
 
     title = schema.TextLine(
-        title=_(u"Name of the app"),
-        required=True,
+        title=_(u'Name of the app'),
+        required=False,
+        default=u'',
     )
     author = schema.TextLine(
-        title=_(u"Contributors of the app"),
-        required=True,
+        title=_(u'Author of the app'),
+        required=False,
+        default=u'',
     )
     price = schema.TextLine(
-        title=_(u"Price of the app in store's"),
-        required=True,
+        title=_(u'Price of the app in stores'),
+        required=False,
+        default=u'FREE',
     )
     icon = schema.TextLine(
-        title=_(u"Icon used in banner"),
+        title=_(u'Icon used in banner'),
         description=_(u'Path to default image'),
         default=u'/logo.png',
         required=False,
     )
+    button_text = schema.TextLine(
+        title=_(u'Text on button'),
+        required=False,
+        default=u'View',
+    )
     ios_url = schema.TextLine(
-        title=_(u"Url app in Apple store"),
-        required=True,
+        title=_(u'Url app in Apple store'),
+        required=False,
+        default=u'',
     )
     android_url = schema.TextLine(
-        title=_(u"Url app in Google play"),
-        required=True,
+        title=_(u'Url app in Google play'),
+        required=False,
+        default=u'',
     )
-    platforms = schema.TextLine(
-        title=_(u"Enable platforms"),
-        description=_(u"android,ios"),
-        required=True,
+    platforms = schema.List(
+        title=_(u'Enable platforms'),
+        description=_(u'Can be used to temporarily disable a platform '
+                      'without needing to remove the url.'),
+        required=False,
+        value_type=schema.Choice(values=PLATFORMS),
+        default=PLATFORMS,
     )
--- src/collective/smartappbanner/browser/viewlets.py
+++ src/collective/smartappbanner/browser/viewlets.py
@@ -14,6 +14,18 @@ class smartappbannerViewlet(ViewletBase):
                 IRegistry).forInterface(ISmartappbannerBaseSettings)  # noqa
         except (ComponentLookupError, KeyError):
             return
+        # Do we have enabled platforms?
+        platforms = self.settings.platforms
+        if not platforms:
+            return
+        # Do we have a url for at least one enabled platform?
+        android_url = self.settings.android_url
+        ios_url = self.settings.ios_url
+        has_ios = u'ios' in platforms and ios_url
+        has_android = u'android' in platforms and android_url
+        if not (has_android or has_ios):
+            return
+
         tags = {}
 
         tags['smartbanner:title'] = self.settings.title
@@ -23,9 +35,10 @@ class smartappbannerViewlet(ViewletBase):
         tags['smartbanner:price-suffix-google'] = 'In Google Play'
         tags['smartbanner:icon-apple'] = self.settings.icon
         tags['smartbanner:icon-google'] = self.settings.icon
-        tags['smartbanner:button'] = 'VIEW'
-        tags['smartbanner:button-url-apple'] = self.settings.ios_url
-        tags['smartbanner:button-url-google'] = self.settings.android_url
-        tags['smartbanner:enabled-platforms'] = self.settings.platforms
+        tags['smartbanner:button'] = self.settings.button_text
+        tags['smartbanner:button-url-apple'] = ios_url
+        tags['smartbanner:button-url-google'] = android_url
+        tags['smartbanner:enabled-platforms'] = ','.join(
+            self.settings.platforms)
 
         return tags
