9dd8d88c281febd9443e1b998bec8ce0107f660c
Maurits van Rees <maurits@vanrees.org>
2017-05-17 11:23:41 +0200

    Back to development: 1.0.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.smartappbanner',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description="Smart app banners on Plone for iOS, Android, and Windows.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
